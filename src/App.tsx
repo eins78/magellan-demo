bf533b5bfebc46f19e75db8788701f2b7ded0327
Max F. Albrecht <1@178.is>
2022-09-09 22:34:51 +0200

    add "greetMe" service function

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { greetMe } from "./services/greet-me";
 
 function App() {
+  const [greeting, setGreeting] = useState("... waiting for greetings ...");
+
+  useEffect(() => {
+    greetMe("React")
+      .then(setGreeting)
+      .catch((reason) => {
+        console.error(`Error requesting greeting: ${reason}`);
+        setGreeting("server does not wish to greet us");
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{greeting}</p>
       </header>
     </div>
   );
---
+++ src/services/greet-me.ts
@@ -0,0 +1,4 @@
+export const greetMe = async (name: string): Promise<string> => {
+  // In the browser, accessing process.arch cause an error.
+  return `Hello ${name}, great to see you on ${typeof window === "undefined" ? `${process.arch} server` : "browser"}!`;
+};
